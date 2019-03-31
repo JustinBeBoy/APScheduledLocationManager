10e98135899849c111f86387c679be7a07848ea5
Karl Cosse <karl.cosse@orange.com>
2019-04-01 01:25:31 +0200

    Migrate to swift 4.2 Reformat

--- APScheduledLocationManagerTests/APScheduledLocationManagerTests.swift
+++ APScheduledLocationManagerTests/APScheduledLocationManagerTests.swift
@@ -10,10 +10,8 @@ import XCTest
 @testable import APScheduledLocationManager
 
 class APScheduledLocationManagerTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
 }
--- ScheduledLocationExample/ViewController.swift
+++ ScheduledLocationExample/ViewController.swift
@@ -11,54 +11,48 @@ import APScheduledLocationManager
 import CoreLocation
 
 class ViewController: UIViewController, APScheduledLocationManagerDelegate {
-    
     private var manager: APScheduledLocationManager!
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var textView: UITextView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         manager = APScheduledLocationManager(delegate: self)
     }
-    
+
     @IBAction func startStop(_ sender: AnyObject) {
-        
+
         if manager.isRunning {
-            
             startStopButton.setTitle("start", for: .normal)
             manager.stopUpdatingLocation()
-        }else{
-            
+        } else {
             if CLLocationManager.authorizationStatus() == .authorizedAlways {
-                
                 startStopButton.setTitle("stop", for: .normal)
-                manager.startUpdatingLocation(interval: 5, acceptableLocationAccuracy: 100)
-            }else{
-                
+                let interval: Double = 5
+                let acceptableLocationAccuracy: Double = 100
+                manager.startUpdatingLocation(interval: interval, acceptableLocationAccuracy: acceptableLocationAccuracy)
+                textView.text = "startUpdatingLocation : interval \(interval) sec | acceptableLocationAccuracy \(acceptableLocationAccuracy)"
+            } else {
                 manager.requestAlwaysAuthorization()
             }
         }
     }
-    
+
     func scheduledLocationManager(_ manager: APScheduledLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        
-        print("didUpdateLocations",locations.last)
+
+        print("didUpdateLocations", locations.last!)
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         formatter.timeStyle = .medium
-        
+
         let l = locations.first!
-        
+
         textView.text = "\(textView.text!)\r \(formatter.string(from: Date())) loc: \(l.coordinate.latitude), \(l.coordinate.longitude)"
     }
-    
+
     func scheduledLocationManager(_ manager: APScheduledLocationManager, didFailWithError error: Error) {
-        
     }
-    
+
     func scheduledLocationManager(_ manager: APScheduledLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        
     }
 }
